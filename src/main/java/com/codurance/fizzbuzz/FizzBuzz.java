279906e8e371433956f59989c1f5d460020c58be
Tom Spencer <tom@homeflow.co.uk>
2020-03-02 11:20:03 +0000

    Going back to use rule of three in implementing fizzBuzz solution.

--- src/main/java/com/codurance/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/codurance/fizzbuzz/FizzBuzz.java
@@ -1,25 +1,7 @@
 package com.codurance.fizzbuzz;
 
 public class FizzBuzz {
-  private int number;
-
-  public static void main(String[] args) {
-    FizzBuzz fizzBuzz = new FizzBuzz();
-    for (int i = 1; i <= 100; i++) {
-      System.out.println(fizzBuzz.convert(i));
-    }
-  }
-
-  public String convert(int fizzNumber) {
-    number = fizzNumber;
-    String result = "";
-
-    if (multipleOf(3)) result += "fizz";
-    if (multipleOf( 5)) result += "buzz";
-    return result == "" ? String.valueOf(number) : result;
-  }
-
-  private boolean multipleOf(int i) {
-    return number % i == 0;
+  public String convert(int i) {
+    return "1";
   }
 }
--- src/test/java/com/codurance/fizzbuzz/FizzBuzzShould.java
+++ src/test/java/com/codurance/fizzbuzz/FizzBuzzShould.java
@@ -1,38 +1,18 @@
 package com.codurance.fizzbuzz;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
 
 public class FizzBuzzShould {
   FizzBuzz fizzBuzz;
   @Before
-  public void setUp() {
+  public void setUp() throws Exception {
     fizzBuzz = new FizzBuzz();
   }
 
   @Test
-  public void return_number_if_not_multiple_of_three_or_five() {
-    int number = 1;
-    assertEquals(String.valueOf(number), fizzBuzz.convert(number));
-  }
-
-  @Test
-  public void return_fizz_for_a_multiple_of_three() {
-    int number = 3;
-    assertEquals("fizz", fizzBuzz.convert(3));
-  }
-
-  @Test
-  public void return_buzz_for_a_multiple_of_five() {
-    int number = 5;
-    assertEquals("buzz", fizzBuzz.convert(number));
-  }
-
-  @Test
-  public void returns_fizzbuzz_for_a_multiple_of_15() {
-    int number = 15;
-    assertEquals("fizzbuzz", fizzBuzz.convert(number));
+  public void return_one_for_one() {
+    Assert.assertEquals("1", fizzBuzz.convert(1));
   }
 }

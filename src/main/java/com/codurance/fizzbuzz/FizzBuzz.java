602747de3c37c185207c074c6881f3f739cf93ae
Tom Spencer <tom@homeflow.co.uk>
2020-03-02 10:18:57 +0000

    Add dynamic composition for string result.

--- src/main/java/com/codurance/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/codurance/fizzbuzz/FizzBuzz.java
@@ -1,11 +1,13 @@
 package com.codurance.fizzbuzz;
 
 public class FizzBuzz {
-
   public String convert(int number) {
-    if (number % 15 == 0) return "fizzbuzz";
-    if (number % 5 == 0) return "buzz";
-    if (number % 3 == 0) return "fizz";
-    return String.valueOf(number);
+    String result = "";
+    if (number % 3 == 0) result += "fizz";
+    if (number % 5 == 0) result += "buzz";
+    if (result == "")
+      return String.valueOf(number);
+
+    return result;
   }
 }

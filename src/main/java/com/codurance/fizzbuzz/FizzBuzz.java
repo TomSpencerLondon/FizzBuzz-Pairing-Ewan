fc854558423f31bf98753593410938d64a2b4bf0
Tom Spencer <tom@homeflow.co.uk>
2020-03-02 10:25:24 +0000

    main method prints 100 fizzbuzzes

--- src/main/java/com/codurance/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/codurance/fizzbuzz/FizzBuzz.java
@@ -1,6 +1,13 @@
 package com.codurance.fizzbuzz;
 
 public class FizzBuzz {
+  public static void main(String[] args) {
+    FizzBuzz fizzBuzz = new FizzBuzz();
+    for (int i = 1; i <= 100; i++) {
+      System.out.println(fizzBuzz.convert(i));
+    }
+  }
+
   public String convert(int number) {
     String result = "";
     if (number % 3 == 0) result += "fizz";
